d5990299e019b6bc908ff73f4311e6509af77db2
shuraaok <aleksandrakutepovaa@gmail.com>
2025-09-20 13:24:26 +0200

    add

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b;
+    c = sqrt(a*a + b*b);
+    d = a + b + c;
+    cout << c << endl;
+    cout << d << endl;
+}
